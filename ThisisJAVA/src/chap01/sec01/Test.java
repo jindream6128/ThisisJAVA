8b2772b0a93914c93b6ff75d8717753add8e6743
jindream6128 <jindream6128@naver.com>
2022-11-27 23:44:51 +0900

    test

--- ThisisJAVA/src/chap01/sec01/Test.java
+++ ThisisJAVA/src/chap01/sec01/Test.java
@@ -1,9 +1,9 @@
 package chap01.sec01;
 
-public class hello2 {
+public class Test {
 
 	public static void main(String[] args) {
-System.out.println("hello2");
+		System.out.println("Test");
 	}
 
 }
