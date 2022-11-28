2049e98c42d7372ba468c49830436ca4f8cb2fe9
jindream6128 <jindream6128@naver.com>
2022-11-29 01:55:19 +0900

    Chapter 01 ~ Chapter 02.01

--- ThisisJAVA/src/chap01/sec01/Hello.java
+++ ThisisJAVA/src/chap01/sec01/Hello.java
@@ -1,10 +1,7 @@
 package chap01.sec01;
 
 public class Hello {
-
 	public static void main(String[] args) {
-		System.out.println("Hello");
-
+		System.out.println("Hello, Java2");
 	}
-
 }
---
+++ ThisisJAVA/src/chap02/sec01/VariableInitializationExample.java
@@ -0,0 +1,12 @@
+package chap02.sec01;
+
+public class VariableInitializationExample {
+
+	public static void main(String[] args) {
+		int value;
+		value =5;
+		int result = value + 5;
+		System.out.println(result);
+	}
+
+}
