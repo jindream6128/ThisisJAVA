7f98211ea8bf5ed50c9c5f9719a2ed20d9c51308
jindream6128 <jindream6128@naver.com>
2022-12-31 02:13:56 +0900

    이것이 자바다 chapter6

---
+++ ThisisJAVA/src/chap06/sec08/exam03/CarExample.java
@@ -0,0 +1,16 @@
+package chap06.sec08.exam03;
+
+public class CarExample {
+
+	public static void main(String[] args) {
+		Car car = new Car();
+		
+		car.setGas(5);
+		
+		if(car.isLeftGas()) {
+			System.out.println("출발 합니다");
+		}
+		car.run();
+	}
+
+}
